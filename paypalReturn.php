a75fd475bcba03f7370ddc5a6ccd15cae5a14d0d
bobvanluijt <bob@vanluijt.nl>
2014-09-24 10:34:34 +0200

    General update

--- paypalReturn.php
+++
@@ -1,9 +0,0 @@
-<?php
-//
-// Returned info from Paypal
-//
-include_once('./Classes/BBoilerplate_Core.php');
-BBoilerplate::run();
-BBoilerplate_Paypal::setConsent(htmlspecialchars($_GET['code']));
-echo 'return';
-header('Location: /profile');
--- redirect.php
+++
@@ -1,5 +0,0 @@
-<?php
-//
-//index functions as redirect to dashboard
-//
-header('Location: https://read.deeg.in/dashboard');
